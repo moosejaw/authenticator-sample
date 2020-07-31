6c5b242087aa3b58036585dfead0652fb5709b42
moosejaw <joshdemir@gmail.com>
2020-07-31 19:51:54 +0100

    Create login form and base template

--- app/app.py
+++ app/app.py
@@ -4,6 +4,25 @@ import flask
 app = flask.Flask(__name__)
 
 
-@app.route("/")
+@app.route('/')
 def index():
-    return flask.render_template("index.html")
+    return flask.render_template(os.path.join('index', 'index.html'))
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    '''
+    Returns the login form on GET, and attempts to log in the user on POST.
+    '''
+    if flask.request.method == 'GET':
+        return flask.render_template(
+            os.path.join('login', 'login.html'),
+            login_page=True
+        )
+    else:
+        pass
+
+
+foo = 'hello'
+if foo != 'hello':
+    pass
