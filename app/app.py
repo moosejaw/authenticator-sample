76dc318834a6710b2c3980f0d940837400a05863
moosejaw <joshdemir@gmail.com>
2020-08-01 17:41:00 +0100

    Add user login functionality

--- app/app.py
+++ app/app.py
@@ -1,7 +1,27 @@
 import os
 import flask
+import requests
+import markupsafe
+import flask_login
+from modules.user import User
+
 
 app = flask.Flask(__name__)
+app.secret_key = os.urandom(16)  # disposable key for convenience sake
+login_mgr = flask_login.LoginManager()
+login_mgr.init_app(app)
+
+session_users = {}  # stores the active user sessions
+
+
+@login_mgr.user_loader
+def load_user(user_id):
+    if user_id is not None:
+        try:
+            return session_users[user_id]
+        except KeyError:
+            return None
+    return None
 
 
 @app.route('/')
@@ -14,15 +34,44 @@ def login():
     '''
     Returns the login form on GET, and attempts to log in the user on POST.
     '''
-    if flask.request.method == 'GET':
+    def render_page(failed_login=None):
+        '''
+        Renders the login form in a browser. When failed is not None,
+        it is a tuple containing the HTTP status code and error message
+        from the failed login attempt.
+        '''
         return flask.render_template(
             os.path.join('login', 'login.html'),
-            login_page=True
+            failed_login=failed_login
         )
+
+    if flask.request.method == 'GET':
+        return render_page()
     else:
-        pass
+        res = requests.post(
+            f'http://{os.environ["AUTH_DNS"]}:{os.environ["AUTH_PORT"]}/login',
+            json={
+                'username': flask.request.form['username'],
+                'password': flask.request.form['password']
+            }
+        )
+        if res.status_code == 200:
+            # Create a new 'user', log them in with flask-login
+            # and set the JWT as the 'token' property
+            user_name = markupsafe.escape(flask.request.form['username'])
 
+            # For convenience, the user_name is the same as the user_id
+            # (except the user_id is converted to unicode as required by
+            # flask-login)
+            user = User(res.json()['token'], user_name)
+            user.name = user_name
+            session_users[user.id] = user
+            flask_login.login_user(user)
 
-foo = 'hello'
-if foo != 'hello':
-    pass
+            # Return the user to index for now
+            flask.flash('Success! You are now logged in.')
+            return flask.redirect(flask.url_for('index'))
+        else:
+            # Non-200 from authenticator, reload login form w/ error message
+            print(f'Failed login attempt: ({res.status_code}) {res.text}')
+            return render_page(failed_login=(res.status_code, res.text))
